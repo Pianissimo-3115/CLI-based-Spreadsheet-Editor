79a7136459d3b7cf695911c2a7ce915edc1c3efa
viraaz11 <155564768+viraaz11@users.noreply.github.com>
2025-03-02 00:55:22 +0530

    negative_in_sleep updated

--- evaluate_operations.c
+++ evaluate_operations.c
@@ -16,7 +16,6 @@ int min_eval(Cell** data, Cell_func* func, int C){
                 return INT_MIN;
             }
             mini = min(mini,(*(data + C*(j-1) + i - 1))->value);
-            // (*(data + C*(i - 1) + j -1))->children->root=insert((*(data + C*(i - 1) + j -1))->children->root,cell);
         }        
     }
     return mini;
@@ -32,7 +31,6 @@ int max_eval(Cell** data, Cell_func* func, int C){
                 return INT_MIN;
             }
             maxi = max(maxi,(*(data + C*(j-1) + i - 1))->value);
-            // (*(data + C*(i - 1) + j -1))->children->root=insert((*(data + C*(i - 1) + j -1))->children->root,cell);
         }        
     }
     return maxi;
@@ -48,7 +46,6 @@ int sum_eval(Cell** data, Cell_func* func, int C){
                 return INT_MIN;
             }
             sum += (*(data + C*(j - 1) + i - 1))->value;
-            // (*(data + C*(i - 1) + j -1))->children->root=insert((*(data + C*(i - 1) + j -1))->children->root,cell);
         }        
     }
     return sum;
@@ -65,7 +62,6 @@ int avg_eval(Cell** data, Cell_func* func, int C){
                 return INT_MIN;
             }
             sum += (*(data + C*(j - 1) + i - 1))->value;
-            // (*(data + C*(i - 1) + j -1))->children->root=insert((*(data + C*(i - 1) + j -1))->children->root,cell);
             count++;
         }        
     }
@@ -84,7 +80,6 @@ int stdev_eval(Cell** data, Cell_func* func, int C){
             }
             count++;
             sum += (*(data + C*(j - 1) + i - 1))->value;
-            // (*(data + C*(i - 1) + j -1))->children->root=insert((*(data + C*(i - 1) + j -1))->children->root,cell);
         }        
     }
     long double mean = sum/count;
@@ -101,34 +96,6 @@ void sleep(int seconds) {
     while ((time(NULL) - start_time) < seconds);
 }
 
-
-
-// void  update_depth(Cell** data, Cell* cell, int R, int C){
-//     int old_depth=cell->depth;
-//     Cell_func* func=cell->func;
-//     if(func->op>5) {            // is a range function
-//         Cell* start=func->Cell1;
-//         Cell* end=func->Cell2;
-//         for(int col=start->col_name;col<=end->col_name;col++){
-//             for(int row=start->row_num;row<=end->row_num;row++){
-//                 cell->depth=max(cell->depth,(*(data + C*(row - 1) + col - 1))->depth+1);
-//             }
-//         }
-//     }
-//     else{
-//         bool done=false;
-//         if(func->flag1) {
-//             cell->depth=max(cell->depth,func->Cell1->depth+1);
-//             done=true;
-//         }
-//         if(func->flag2) {
-//             cell->depth=max(cell->depth,func->Cell2->depth+1);
-//             done=true;
-//         }
-//         if(!done) cell->depth=0;
-//     }
-//     return;
-// }
 void remove_old_dependencies(Cell** data, Cell_func* old_func, Cell* cell, int C){
     if(old_func->op>5){             // is a range function
         Cell* start=old_func->Cell1;
@@ -173,22 +140,16 @@ void calculate(Cell** data, Cell* cell, int C) {
         case ADD: {
             if((func->flag1 && (func->Cell1->valid == 0)) || (func->flag2 && (func->Cell2->valid == 0))) cell->valid = 0;
             else cell->value = val1 + val2;
-            // if(func->flag1) func->Cell1->children->root = insert(func->Cell1->children->root, cell);
-            // if(func->flag2) func->Cell2->children->root = insert(func->Cell2->children->root, cell);
             break;
         }
         case SUB: {
             if((func->flag1 && (func->Cell1->valid == 0)) || (func->flag2 && (func->Cell2->valid == 0))) cell->valid = 0;
             else cell->value = val1 - val2; 
-            // if(func->flag1) func->Cell1->children->root = insert(func->Cell1->children->root, cell);
-            // if(func->flag2) func->Cell2->children->root = insert(func->Cell2->children->root, cell);
             break;
         }
         case MUL: {
             if((func->flag1 && (func->Cell1->valid == 0)) || (func->flag2 && (func->Cell2->valid == 0))) cell->valid = 0;
             else cell->value = val1*val2; 
-            // if(func->flag1) func->Cell1->children->root = insert(func->Cell1->children->root, cell);
-            // if(func->flag2) func->Cell2->children->root = insert(func->Cell2->children->root, cell);
             break;
         }
         case DIV: {
@@ -196,22 +157,19 @@ void calculate(Cell** data, Cell* cell, int C) {
                 cell->valid = 0; 
                 break;
             }
-            if(val2 == 0) cell->valid = 0;          //////////////////////////////////////////////////////// TO BE DISCUSSED IF WE HAVE TO PUT IN DEPENDENCY SET IN THIS CASE
+            if(val2 == 0) cell->valid = 0;          
             else {
                 cell->value = val1/val2;
-                // if(func->flag1) func->Cell1->children->root = insert(func->Cell1->children->root, cell);
-                // if(func->flag2) func->Cell2->children->root = insert(func->Cell2->children->root, cell);  
             }            
             break;
         }
-        case SLEEP: {                       ///////////////////////////////////////////////////////////////// HAVE TO MODIFY THIS: SLEEP CAN ALSO REQUIRE ADDRESS INSTEAD OF FIX VAL. ALSO INSERT IN AVL TREE IN THAT CASE
+        case SLEEP: {        
             if((func->flag1 && (func->Cell1->valid == 0)) || (func->flag2 && (func->Cell2->valid == 0))) {
                 cell->valid = 0; 
                 break;
             }
             sleep(val1);
             cell->value = val1;
-            // if(func->flag1) func->Cell1->children->root = insert(func->Cell1->children->root, cell);
             break;
         }
         case MIN:
@@ -243,66 +201,23 @@ void calculate(Cell** data, Cell* cell, int C) {
             break;
     }
 }
-void update_parent_avls(Cell** data, Cell *cell, int C /*, Cell_func* old_func*/){
+void update_parent_avls(Cell** data, Cell *cell, int C){
     Cell_func* func=cell->func;
     if(func==NULL) return;
-    // bool was_range_func;
-    // if(old_func!=NULL&&old_func->op>5) was_range_func=true;
-    // else was_range_func=false;
     if(func->op>5) {          // is a range function
         Cell* cell1=func->Cell1;
         Cell* cell2=func->Cell2;
         for(int col=cell1->col_name; col<=cell2->col_name; col++){
             for(int row=cell1->row_num; row<=cell2->row_num; row++){
-                // if(was_range_func && 
-                //     old_func->Cell1->col_name<=col && 
-                //     old_func->Cell2->col_name>=col && 
-                //     old_func->Cell1->row_num<=row && 
-                //     old_func->Cell2->row_num>=row) continue;
-
-                // if(old_func!=NULL && 
-                //     !was_range_func && 
-                //     old_func->flag1 &&
-                //     old_func->Cell1->col_name==col &&
-                //     old_func->Cell1->row_num==row) continue;
-                    
-                // if(old_func!=NULL && 
-                //     !was_range_func && 
-                //     old_func->flag2 && 
-                //     old_func->Cell2->col_name==col && 
-                //     old_func->Cell2->row_num==row) continue;
-
                 (*(data + C*(row - 1) + col -1))->children->root=insert((*(data + C*(row - 1) + col -1))->children->root,cell);
             }
         }
     }
     else{
         if(func->flag1) {
-            // if(was_range_func && 
-            //     old_func->Cell1->col_name<=func->Cell1->col_name && 
-            //     old_func->Cell2->col_name>=func->Cell1->col_name && 
-            //     old_func->Cell1->row_num<=func->Cell1->row_num && 
-            //     old_func->Cell2->row_num>=func->Cell1->row_num) goto skip;
-
-            // if(old_func!=NULL && 
-            //     !was_range_func && 
-            //     old_func->flag1 && 
-            //     old_func->Cell1->col_name==func->Cell1->col_name && 
-            //     old_func->Cell1->row_num==func->Cell1->row_num) goto skip;
-
-            // if(old_func!=NULL && 
-            //     !was_range_func && 
-            //     old_func->flag2 && 
-            //     old_func->Cell2->col_name==func->Cell1->col_name && 
-            //     old_func->Cell2->row_num==func->Cell1->row_num) goto skip;
-
             func->Cell1->children->root=insert(func->Cell1->children->root,cell);
         }
-        // skip:
         if(func->flag2) {
-            // if(was_range_func && old_func->Cell1->col_name<=func->Cell2->col_name && old_func->Cell2->col_name>=func->Cell2->col_name && old_func->Cell1->row_num<=func->Cell2->row_num && old_func->Cell2->row_num>=func->Cell2->row_num) return;
-            // if(old_func!=NULL && !was_range_func && old_func->flag1 && old_func->Cell1->col_name==func->Cell2->col_name && old_func->Cell1->row_num==func->Cell2->row_num) return;
-            // if(old_func!=NULL && !was_range_func && old_func->flag2 && old_func->Cell2->col_name==func->Cell2->col_name && old_func->Cell2->row_num==func->Cell2->row_num) return;
             func->Cell2->children->root=insert(func->Cell2->children->root,cell);
         }
     }
@@ -357,67 +272,56 @@ ll_Node* topological_sort(Cell* current_cell){
     HashTable* visited = create_table(TABLE_SIZE);
     HashTable* recStack = create_table(TABLE_SIZE);
     Stack *stack = createStack();
-    // ll_Node** head = (ll_Node**)malloc(sizeof(ll_Node*));
-    // ll_Node** temp = (ll_Node**)malloc(sizeof(ll_Node*));
-    // *temp = NULL;
-    // *head = NULL;
-    // inorder(current_cell->children->root, temp);
     if (dfs(current_cell, visited, recStack, stack)) {
         freeStack(stack);
         free_table(visited);
         free_table(recStack);
         if(stack) free(stack);
-        // free(head);
         return NULL;
     }
-    // while(*temp != NULL){
-    //     if(!hash_search(visited, (*temp)->data)){
-    //         if (dfs((*temp)->data, visited, recStack, stack)) {
-    //             free(stack);
-    //             return NULL;
-    //         }
-    //     }
-    //     (*temp) = (*temp)->next;
-    // }
-
-    // while(!isEmpty(stack)) {
-    //     insertAtHead(head,pop(stack));
-    // }
-    // insertAtHead(head, current_cell);
-    // freeLinkedList((*temp));
-    // free(temp);
     free_table(visited);
     free_table(recStack);
     ll_Node* ret=stack->top;
-    // freeStack(stack);
-
     if(stack) free(stack);
-    // free(head);
     return ret;
 }
 
 int update_children(Cell** data, Cell* cell, int C) {
     ll_Node* head = topological_sort(cell);
     if(head == NULL) return 0;
+    bool negative_in_sleep = false;
     while (head != NULL) {
         Cell* element = head->data;
-        calculate(data, element, C);
+        if(element->func != NULL && element->func->op == SLEEP) {
+            if(element->func->flag1 && element->func->Cell1->value < 0) {
+                negative_in_sleep = true;
+            }
+            else if(element->func->flag2 && element->func->Cell2->value < 0) {
+                negative_in_sleep = true;
+            }
+            else if(element->func->value1 < 0) {
+                negative_in_sleep = true;
+            }
+        }
+        if(!negative_in_sleep) {
+            calculate(data, element, C);    
+        }
         ll_Node* temp = head;
         head = head->next;
         free(temp);
+    }
+    if(negative_in_sleep) {
+        return 0;
     }
     return 1;
 }
 
 
 int evaluate(Cell** data, Cell *cell, Cell_func* old_func, int R ,int C) { 
-    // int initial_value=cell->value;
     if(old_func!=NULL){
         remove_old_dependencies(data, old_func, cell, C);
     }
-    update_parent_avls(data,cell,C/*,old_func*/);
-    // if(old_func!=NULL) free(old_func);
-
+    update_parent_avls(data,cell,C);
     // this func would take care of updating the children of the cell and also detect cycle if any
     int x = update_children(data, cell, C); 
 
@@ -430,83 +334,3 @@ int evaluate(Cell** data, Cell *cell, Cell_func* old_func, int R ,int C) {
     if(old_func!=NULL) free(old_func);
     return x;
 }
-
-// int main(){
-//     Cell** data = (Cell**)malloc(5 * sizeof(Cell*));
-//     for (int i = 0; i < 5; i++) {
-//         data[i] = (Cell*)malloc(5 * sizeof(Cell));
-//         for (int j = 0; j < 5; j++) {
-//             data[i][j].value = i * 5 + j;
-//             data[i][j].col_name = j + 1;
-//             data[i][j].row_num = i + 1;
-//             data[i][j].valid = 1;
-//             data[i][j].func = NULL;
-//             data[i][j].children = (AVL*)malloc(sizeof(AVL));
-//             data[i][j].children->root = NULL;
-//         }
-//     }
-//     Cell_func* func = (Cell_func*)malloc(sizeof(Cell_func));
-//     func->op = ADD;
-//     func->flag1 = 1;
-//     func->flag2 = 1;
-//     func->Cell1 = &data[0][1];
-//     func->Cell2 = &data[1][0];
-//     data[0][0].func = func;     // A1 = B1 + A2
-//     evaluate(data, &data[0][0], NULL, 5, 5);
-//     for (int i = 0; i < 5; i++) {
-//         for (int j = 0; j < 5; j++) {
-//             printf("%d ", data[i][j].value);
-//         }
-//         printf("\n");
-//     }
-
-//     Cell_func* func2 = (Cell_func*)malloc(sizeof(Cell_func));
-//     func2->op = ADD;
-//     func2->flag1 = 1;
-//     func2->flag2 = 1;
-//     func2->Cell1 = &data[0][0];
-//     func2->Cell2 = &data[1][1];
-//     data[1][0].func = func2;     // B1 = A1 + B2
-//     evaluate(data, &data[0][0], func, 5, 5);
-//     for (int i = 0; i < 5; i++) {
-//         for (int j = 0; j < 5; j++) {
-//             printf("%d ", data[i][j].value);
-//         }
-//         printf("\n");
-//     }
-
-//     Cell_func* func3 = (Cell_func*)malloc(sizeof(Cell_func));
-//     func3->op = ADD;
-//     func3->flag1 = 1;
-//     func3->flag2 = 1;
-//     func3->Cell1 = &data[0][2];
-//     func3->Cell2 = &data[1][2];
-//     data[1][1].func = func3;     // B2 = A2 + B3
-//     evaluate(data, &data[0][0], func2, 5, 5);
-//     for (int i = 0; i < 5; i++) {
-//         for (int j = 0; j < 5; j++) {
-//             printf("%d ", data[i][j].value);
-//         }
-//         printf("\n");
-//     }
-
-//     Cell_func* func4 = (Cell_func*)malloc(sizeof(Cell_func));
-//     func4->op = ADD;
-//     func4->flag1 = 1;
-//     func4->flag2 = 1;
-//     func4->Cell1 = &data[0][3];
-//     func4->Cell2 = &data[1][3];
-//     data[1][2].func = func4;     // B3 = A3 + B4
-//     evaluate(data, &data[0][0], func3, 5, 5);
-//     for (int i = 0; i < 5; i++) {
-//         for (int j = 0; j < 5; j++) {
-//             printf("%d ", data[i][j].value);
-//         }
-//         printf("\n");
-//     }
-
-
-
-//     printf("\n");
-//     printf("Done\n");
-// }
